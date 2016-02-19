5e557d6e27b5bb1dcfb238324af89da0024ed5c9
AllanCaetano <allansantos_caetano@hotmail.com>
2016-02-19 14:12:24 -0200

    Update ajudantes.php

--- Contatos/ajudantes.php
+++ Contatos/ajudantes.php
@@ -79,9 +79,9 @@ function enviar_email($contato, $anexos = array()) {
     $obj_email->Port = 587;
     $obj_email->SMTPSecure = "tls";
     $obj_email->SMTPAuth = TRUE;
-    $obj_email->Username = "allansantoscaetano@gmail.com";
-    $obj_email->Password = "22917553478";
-    $obj_email->setFrom("allansantoscaetano@gmail.com", "Recordador de Contatos");
+    $obj_email->Username = "email@gmail.com";
+    $obj_email->Password = "senha";
+    $obj_email->setFrom("email@gmail.com", "Recordador de Contatos");
     $obj_email->addAddress(EMAIL_NOTIFICACAO);
     $obj_email->CharSet = "UTF-8";
     $obj_email->Subject = "Contato: {$contato['nome']}";

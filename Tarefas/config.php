6c2ab04af60e87f3fa09f4d8cf21c2f82135a932
AllanCaetano <allansantos_caetano@hotmail.com>
2016-02-19 14:09:07 -0200

    Update config.php

--- Tarefas/config.php
+++ Tarefas/config.php
@@ -7,5 +7,5 @@ define("BD_SENHA", "root");
 define("BD_BANCO", "php_mysql");
 
 // E-mail para notificação
-define("EMAIL_NOTIFICACAO", "allansantos_caetano@hotmail.com");
+define("EMAIL_NOTIFICACAO", "email@provedor.com");
 
